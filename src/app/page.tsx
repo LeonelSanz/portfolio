66d95a1281db76890153bf12b125066c3959feeb
Leonel <leonelsanz15@gmail.com>
2023-06-26 21:57:59 -0300

    header added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+import Header from '@/components/header'
 import HomeSection from '@/components/home'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
-      <HomeSection />
+      <div className='relative'>
+        <HomeSection />
+        <div
+          id='content'
+          className='px-8 relative flex items-center max-w-[1100px] mx-auto'
+        >
+          <div className='h-full text-left max-w-full'>
+            <div className='grid grid-cols-2 gap-2 h-full w-full'>
+              <Header />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import NavHeader from "./nav";
+
+type Props = {};
+
+export default function Header({}: Props) {
+    return (
+        <header className="flex col-span-1 max-[800px]:col-span-2 top-0 h-screen max-[800px]:h-auto min-[800px]:sticky gap-3">
+            <div className="h-full py-24 px-14 max-[800px]:px-2 flex flex-col">
+                <div className="flex flex-col gap-3">
+                    <h1 className="font-bold text-[#1f1c19] dark:text-[#cdbead] relative item-s text-transparent text-6xl bg-clip-text inline-block text-gradient tracking-tighter">
+                        Leonel
+                    </h1>
+                    <h2 className="dark:text-[#ada79f] text-[#6c6761] item-s text-transparent bg-clip-text tracking-tighter text-gradient text-xl">
+                        Full Stack Web Developer
+                    </h2>
+                    <p className="w-96 max-[700px]:w-full opacity-80 dark:text-[#c8c6c0] text-[#302e2a] item-s text-transparent text-gradient bg-clip-text leading-5">
+                        I build accessible products and digital experiences for the web.
+                    </p>
+                </div>
+                <NavHeader />
+                
+            </div>
+        </header>
+    )
+}
