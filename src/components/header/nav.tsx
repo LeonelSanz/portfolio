9b6b47712832d7a9144b997b5f8d583fb024e3e7
Leonel <leonelsanz15@gmail.com>
2023-06-26 21:43:11 -0300

    nav added

---
+++ src/components/header/nav.tsx
@@ -0,0 +1,15 @@
+import NavItem from "./nav-item"
+
+type Props = {}
+
+export default function NavHeader({}: Props) {
+    return (
+        <nav className="my-10 max-[800px]:hidden">
+            <ul className="flex flex-col gap-1 text-base font-semibold">
+                <NavItem href="#about" label="About" />
+                <NavItem href="#projects" label="Projects" />
+                <NavItem href="#skills" label="Skills" />
+            </ul>
+        </nav>
+    )
+}
